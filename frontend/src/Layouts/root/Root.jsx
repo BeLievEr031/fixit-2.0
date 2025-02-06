426f7334f775a88156c4499d436722463ce712a6
BeLieVer031 <sandyrajak031@gmail.com>
2025-02-06 14:50:14 +0530

    add loader

--- frontend/src/Layouts/root/Root.jsx
+++ frontend/src/Layouts/root/Root.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import { self } from "../../http/api";
 import useAuthStore from "../../store/user-store";
 import { useEffect } from "react";
+import Loader from "../../assets/loader.webm";
 // import useAuthStore from "../../store/user-store";
 function Root() {
   // Otherwise Move to auth page
@@ -25,7 +26,17 @@ function Root() {
   }, [data, setUser]);
 
   // Login then move to dashboard
-  return <div>{isLoading ? "Loading...." : <Outlet />}</div>;
+  return (
+    <div>
+      {isLoading ? (
+        <div className="loader">
+          <video src={Loader} autoPlay muted />
+        </div>
+      ) : (
+        <Outlet />
+      )}
+    </div>
+  );
 }
 
 export default Root;
